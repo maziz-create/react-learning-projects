5ec189f091195ab8043deb66a01839ce4e1cc5fa
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-26 23:47:31 +0300

    added input form via received setTodo function and todos state

--- src/components/Todo/Form/index.js
+++ src/components/Todo/Form/index.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
-function Form() {
+const initialFormValues = {
+    name: "",
+    checked: false
+}
+
+function Form({ setTodo, todos }) {
+
+    const [form, setForm] = useState(initialFormValues);
+
+    const onChangeInput = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (form.name === '') {
+            alert("An unexpected error occured..");
+            return false;
+        }
+
+        setTodo([...todos, form])
+        console.log("güncel todos => ", todos);
+    }
+
+    useEffect(() => {
+        setForm(initialFormValues);
+    }, [todos]);
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={onSubmit}>
+            <div>
+                <input
+                    placeholder="Add To Do"
+                    value={form.name}
+                    name="name"
+                    onChange={onChangeInput}
+                    autoComplete="off"
+                />
+            </div>
+        </form>
     )
 }
 
