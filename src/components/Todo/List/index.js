06e2eb8816feb1846bc25d52bea4e4d56d3e64c1
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-26 23:48:20 +0300

    added ul>li elements via received todos state

--- src/components/Todo/List/index.js
+++ src/components/Todo/List/index.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import { useEffect } from 'react';
 
-function List() {
+function List({ todos }) {
+
+    const deleteTodo = (e) => {
+        let li = e.target.parentNode;
+        li.remove();
+    }
+
+    const completeTodo = (e) => {
+        let li = e.target.parentNode;
+        li.classList.add('completed-todo');
+    }
+
+    const deleteMark = (e) => {
+        let li = e.target.parentNode;
+        li.classList.remove('completed-todo');
+    }
+
+    useEffect(() => {
+        console.log("todos state değişti");
+    }, [todos])
+
+
     return (
-        <div>
-            
+        <div id="container">
+            <ul>
+                {
+                    todos.map((todo, index) => {
+                        return <div key={index} className="liDiv">
+                            <li>
+                                {todo.name}
+                                <button className="button-complete" onClick={
+                                    completeTodo
+                                }>
+                                    V
+                                </button>
+                                <button className="button-deleteMark" onClick={
+                                    deleteMark
+
+                                }>
+                                    O
+                                </button>
+                                <button className="button-delete" onClick={
+                                    deleteTodo
+                                }>
+                                    X
+                                </button>
+                            </li>
+                        </div>
+
+                    })
+                }
+            </ul>
         </div>
     )
 }
