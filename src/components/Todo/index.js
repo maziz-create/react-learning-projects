dfabb67985f11f5c6d39f4f8f4b2d0c6a300f197
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-26 23:16:11 +0300

    Added todos state and added initial values into it, added Form and List component integration and passed into todos state from Form Component to List Component

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 import { } from './styles.css';
-import { Form } from './Form';
-import { List } from './List';
+import Form from './Form';
+import List from './List';
 
 function Todo() {
+
+    const [todos, setTodos] = useState([
+        {
+            name: "Todo 1",
+            checked: false
+        },
+        {
+            name: "Todo 2",
+            checked: false
+        },
+        {
+            name: "Todo 3",
+            checked: false
+        }
+    ]);
+
     return (
         <div className="todo">
-            <List />
-            <Form />
+            <Form setTodo={setTodos} todos={todos} />
+            <List todos={todos} />
         </div>
     )
 }
